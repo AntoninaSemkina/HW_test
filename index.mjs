8127a7c75c2925abf38cb116e18a24e0b2ace5f2
Antonina Semkina <antonina.a.semkina@gmail.com>
2024-10-25 14:08:39 +0300

    first commit

---
+++ index.mjs
@@ -0,0 +1,12 @@
+export const fetchUsers = () => {
+  return new Promise((resolve) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => {
+        // console.log(json);
+        resolve(json);
+      });
+  });
+};
+
+fetchUsers();
